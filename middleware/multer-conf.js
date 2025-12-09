9459c743582badd84dd15b5d79ff0e7ab9e2cea5
andrezandu7-oss <andrezandu7@gmail.com>
2025-12-09 21:42:27 +0100

    Create multer-conf.js

---
+++ middleware/multer-conf.js
@@ -0,0 +1,21 @@
+// middleware/multer-config.js
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'images'); // Attention : Ce dossier n'est pas créé par défaut sur Render
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_').split('.')[0];
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+module.exports = multer({storage: storage}).array('photos', 5); // Accepte jusqu'à 5 photos
