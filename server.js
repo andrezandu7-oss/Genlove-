8928ad5e324d118b45d70b94d93871543d386ee0
andrezandu7-oss <andrezandu7@gmail.com>
2026-03-02 20:49:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ============================================
+// ========================
 // CONFIGURAÇÕES
-// ============================================
+// ========================
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static('public'));
 
 // ============================================
 // CONEXÃO MONGODB
