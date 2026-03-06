f827b37ea717b8ea7d8440eef3b2114c818b35a0
andrezandu7-oss <andrezandu7@gmail.com>
2026-03-06 20:06:42 +0100

    Atualizar o server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const cors = require('cors');
 const crypto = require('crypto');
 const PDFDocument = require('pdfkit');
 const path = require('path');
+const QRCode = require('qrcode');
 require('dotenv').config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
